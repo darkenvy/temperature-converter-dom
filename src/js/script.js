97005d193462be180ff41e1c3e24629b408a420a
darkenvy <darkenvy6@gmail.com>
2016-05-27 16:53:08 -0700

    Stylizing

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@ console.log('Hello, front end');
 
 document.addEventListener('DOMContentLoaded', function(){
   // ============ Default function removal ===========
-  // document.getElementById("tmp-input").addEventListener("submit", function(event){
-  // event.preventDefault();
-  // })
+  document.getElementById("tmp-input").addEventListener("enter", function(event){
+  event.preventDefault();
+  })
   document.getElementById("convert-btn").addEventListener("click", function(event){
   event.preventDefault();
   })
@@ -15,12 +15,18 @@ document.addEventListener('DOMContentLoaded', function(){
   // =================================================
   document.getElementById('convert-btn').addEventListener('click', convertTmp);
   document.getElementById('clear-btn').addEventListener('click', clearTmp)
+  convertTmp();
 })
 
 
 function convertTmp(){
   var input = document.getElementById('tmp-input').value;
-  document.getElementById('convertedTemp').textContent = (input - 32) * (5/9);
+  if(document.getElementById('radioF').checked) {
+    document.getElementById('convertedTemp').textContent = (input - 32) * (5/9);
+  } else {
+    document.getElementById('convertedTemp').textContent = (input * (9/5)) + 32;
+  }
+
 }
 
 function clearTmp(){
